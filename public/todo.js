e9fde96c10c0d7389a13b75dec91a7417ab23efb
Holly Bryant <hbryant_98@yahoo.com>
2016-02-18 16:56:09 -0600

    added additional click functionality and 2 functions

--- public/todo.js
+++ public/todo.js
@@ -1,3 +1,45 @@
+
+function deleteItem(itemToDelete) {		
+	$.post("todo-json.php?id="+itemToDelete+"&action=delete",function(item){		
+		console.log(item);
+		$('#thingsToDo').empty();
+		$.get("todo-json.php", function(items) {	//enter different address here	
+			console.log(items);
+		 items.forEach(function(items) {
+		 	date = moment().format("ddd, MMM");
+		 	var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);       			
+		});
+		});
+	});
+}
+
+function completeItem(itemToComplete) {
+	$.post("todo-json.php?id="+itemToComplete+"&action=complete", function(item){
+		console.log(item);
+	$('.done').addClass('disabled');
+	// $('.done').closest('tr').addClass('disabled');
+	});
+}
+
+
+
 $(document).ready(function() {
 console.log("document loaded");
 	$("#btn-new-item").click(function(event) {
@@ -10,23 +52,155 @@ console.log("document loaded");
 			"priority": priority,
 			"due_date": date
 		};
-		$.post("todo-json.php", data, function(item) {		
-			console.log(item);
+		$.post("todo-json.php", data, function(items) {		//enter diff address here
+			console.log(items);
+			$('#content').empty();
+			$('#priority').empty();
+			$('#due_date').empty(); //these are not working
 		});
+
 	});
 
 	$("#btn-show-items").click(function(event) {
 		event.preventDefault(); //prevents page from reloading
-		$.get("todo-json.php", function(items) {		
+		$('#thingsToDo').empty();
+		$.get("todo-json.php", function(items) {	//enter different address here	
 			console.log(items);
-		// $("td").html(content);
-		// $("td").html(priority);
 		 items.forEach(function(items) {
-		 	console.log(items);
-		 	// date = moment().format("ddd, MMM");
-            $('#thingsToDo').append(
-                '<tr><td>' + items.content + '</td><td>' + items.priority + '</td><td>' + items.due_date + '</td><td>');		
+		 	date = moment().format("ddd, MMM");
+		 	var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);       			
+		});
+		});
+	});	
+	$("#btn-sort-by-priority").click(function(event) {
+		event.preventDefault(); //prevents page from reloading
+		$('#thingsToDo').empty();
+		$.get("todo-json.php?order_by=priority", function(items) {	
+			console.log(items);
+		 items.forEach(function(items) {
+		 	var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);  	
+		});
+		});
+	});	
+	$("#btn-sort-by-due_date").click(function(event) {
+		event.preventDefault(); //prevents page from reloading
+		$('#thingsToDo').empty();
+		$.get("todo-json.php?order_by=due_date", function(items) {	
+			console.log(items);
+		 items.forEach(function(items) {
+		 	var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);  			
+		});
 		});
 	});
-	});	
+	$("#btn-sort-by-content").click(function(event) {
+		event.preventDefault(); //prevents page from reloading
+		$('#thingsToDo').empty();
+		$.get("todo-json.php?order_by=content", function(items) {	
+			console.log(items);
+		 items.forEach(function(items) {
+			var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);  			
+		});
+		});
+	});
+	$("#btn-sort-by-complete").click(function(event) {
+		event.preventDefault(); //prevents page from reloading
+		$('#thingsToDo').empty();
+		$.get("todo-json.php?complete=true&order_by=priority", function(items) {	
+			console.log(items);
+		 items.forEach(function(items) {
+				var row = "<tr class='data_row_" + items.id+"'>" +
+					"<td>"+
+						items.content+
+					"</td>"+
+					"<td>"+
+						items.priority+
+					"</td>"+
+					"<td>"+
+						items.due_date+
+					"</td>"+
+					"<td>" + 
+						"<button class='btn btn-danger btn-xs delete' onclick='deleteItem("+items.id+")'>X</button>"+ 
+					"</td>"+
+					"<td>" +
+						"<button class='btn btn-success btn-xs done' onclick='completeItem("+items.id+")'>Complete</button>"+
+					"</td"+	
+				"</tr>";
+            	$('#thingsToDo').append(row);  			
+		});
+		});
+	});
+	
+
+	
+		
 });
+
+
